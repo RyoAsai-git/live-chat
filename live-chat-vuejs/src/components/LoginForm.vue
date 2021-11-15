1f1d6187a88e1aa90c31b5da5535790d9dc4e8b6
Ryo Asai <fu.orangina0516@iCloud.com>
2021-11-15 23:10:35 +0900

    feature:サインアップ機能実装 axiosで非同期にて実装

--- live-chat-vuejs/src/components/LoginForm.vue
+++ live-chat-vuejs/src/components/LoginForm.vue
@@ -1,10 +1,12 @@
 <template>
+<div>
   <h2>ログイン</h2>
   <form>
     <input type="email" required placeholder="メールアドレス" v-model="email">
     <input type="password" required placeholder="パスワード" v-model="password">
     <button>ログインする</button>
   </form>
+</div>
 </template>
 
 <script>
