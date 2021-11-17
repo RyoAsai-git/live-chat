eb9b67c0883ac92734362436817d24889529118f
Ryo Asai <fu.orangina0516@iCloud.com>
2021-11-18 00:29:44 +0900

    fix:チャットページにナビゲーションガード設定

--- live-chat-vuejs/src/router/index.js
+++ live-chat-vuejs/src/router/index.js
@@ -2,6 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Welcome from '../views/Welcome'
 import Chatroom from '../views/Chatroom'
 
+// toは次に表示されるページのルートです
+// fromはtoのページにアクセスしようとしている元のページです
+// nextはそのページにアクセスさせるか、他のページへリダイレクトさせるかなどを操作するためのメソッドです。
+const requireAuth = async (to, from, next) => {
+  console.log('requireAuthが呼ばれています')
+  next()
+}
+
 // 実際のルーティングを定義する箇所
 const routes = [
   {
@@ -13,6 +21,8 @@ const routes = [
     path: '/chatroom',
     name: 'Chatroom',
     component: Chatroom,
+    // 設定されているルートにアクセスする前に実行されるメソッド
+    beforeEnter: requireAuth
   }
 ]
 
