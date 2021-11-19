0b407481c8dfa72cc6f7bf11d68c7672df683c78
Ryo Asai <fu.orangina0516@iCloud.com>
2021-11-19 23:12:50 +0900

    feature:チャットルーム メッセージ取得機能作成

---
+++ live-chat-vuejs/src/components/ChatWindow.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="chat-window">
+    {{ messages }}
+  </div>
+</template>
+
+<script>
+// 親コンポーネントからデータを受け取るにはprops
+// 今回は親コンポーネントのChatroom.vueからmessagesを受け取る必要があるので、props: ['messages']と記述
+export default {
+  props: ['messages'],
+}
+</script>
+
+<style scoped>
+  .chat-window {
+    background: white;
+    padding: 30px 20px;
+    border-bottom: 1px solid #eee;
+  }
+</style>
--- live-chat-vuejs/src/views/Chatroom.vue
+++ live-chat-vuejs/src/views/Chatroom.vue
@@ -1,14 +1,49 @@
 <template>
   <div class="container">
     <Navbar />
+    <ChatWindow :messages="messages" />
   </div>
 </template>
 
 <script>
 import Navbar from '../components/Navbar'
+import ChatWindow from '../components/ChatWindow'
+
+import axios from 'axios'
 
 export default {
-  components: { Navbar },
+  components: { Navbar, ChatWindow },
+  data() {
+    return {
+      message: []
+    }
+  },
+
+  methods: {
+    async getMessages() {
+      try {
+        const res = await axios.get('http://localhost:3000/messages', {
+          headers: {
+            uid: window.localStorage.getItem('uid'),
+            'access-token': window.localStorage.getItem('access-token'),
+            client: window.localStorage.getItem('client')
+          }
+        })
+        if (!res) {
+          new Error('メッセージ一覧を取得できませんでした')
+        }
+        this.message = res.data
+      } catch (err) {
+        console.log(err)
+      }
+    },
+  },
+
+  // mountedはVue.jsの「ライフサイクルフック」の一つ
+  // mountedの場合は、ページが読み込まれHTMLが表示される直前に実行されるメソッドです。
+  mounted() {
+    this.getMessages()
+  }
 }
 </script>
 
